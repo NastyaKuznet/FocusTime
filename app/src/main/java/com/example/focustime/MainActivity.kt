758e4b09e21080f87daed140e8afb6432c27981a
LeeLuiza <Laurabeck1315@gmail.com>
2024-11-08 21:33:10 +0500

    authorization

--- app/src/main/java/com/example/focustime/MainActivity.kt
+++ app/src/main/java/com/example/focustime/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.focustime
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AuthorizationFragment())
+                .commit()
         }
+    }
+
+    fun goToRegister(view: View) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, RegistrationFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun goToLogin(view: View) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, AuthorizationFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
