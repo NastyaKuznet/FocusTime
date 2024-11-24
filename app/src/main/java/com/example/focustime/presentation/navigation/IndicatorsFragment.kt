1faab764a37c28ac71478815ba41b77cb098c9d5
LeeLuiza <Laurabeck1315@gmail.com>
2024-11-24 17:56:45 +0500

    layout

---
+++ app/src/main/java/com/example/focustime/presentation/navigation/IndicatorsFragment.kt
@@ -0,0 +1,41 @@
+package com.example.focustime.presentation.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.focustime.R
+
+class IndicatorsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_indicators, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Инициализация Spinner
+        val periodSpinner: Spinner = view.findViewById(R.id.periodSpinner)
+
+        // Создание массива строк для элементов Spinner
+        val periods = arrayOf("За день", "За месяц", "За год")
+
+        // Создание адаптера для Spinner
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, periods)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Установка адаптера для Spinner
+        periodSpinner.adapter = adapter
+    }
+}
--- app/src/main/java/com/example/focustime/presentation/root/RootFragment.kt
+++ app/src/main/java/com/example/focustime/presentation/root/RootFragment.kt
@@ -3,13 +3,15 @@ package com.example.focustime.presentation.root
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.focustime.R
-import com.example.focustime.databinding.FragmentAuthorizationBinding
 import com.example.focustime.databinding.FragmentRootBinding
 import com.example.focustime.di.ViewModelFactory
-import com.example.focustime.presentation.authorization.AuthorizationUserFragmentViewModel
+import com.example.focustime.presentation.navigation.FocusFragment
+import com.example.focustime.presentation.navigation.FriendsFragment
+import com.example.focustime.presentation.navigation.IndicatorsFragment
+import com.example.focustime.presentation.navigation.TypesIndicatorsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import javax.inject.Inject
 
 class RootFragment: Fragment(R.layout.fragment_root) {
@@ -23,12 +25,31 @@ class RootFragment: Fragment(R.layout.fragment_root) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        with(binding){
+        super.onViewCreated(view, savedInstanceState)
+
+        /*with(binding){
             val userId = arguments?.getInt("userId")
             content.text = "Welcome!\nТвой id: $userId"
-        }
+        }*/
 
-        super.onViewCreated(view, savedInstanceState)
+        makeCurrentFragment(IndicatorsFragment())
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_focus -> makeCurrentFragment(FocusFragment())
+                R.id.nav_friends -> makeCurrentFragment(FriendsFragment())
+                R.id.nav_indicator -> makeCurrentFragment(IndicatorsFragment())
+                R.id.nav_create_indicator -> makeCurrentFragment(TypesIndicatorsFragment())
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
     }
 
 }
